d98cf6eee92dff814cefdac2e9b2af6edd4bb6be
김성은 <ss_eun97@naver.com>
2020-04-18 17:39:15 +0900

    과제1 - Navigation 활용 화면 이동

---
+++ iOS_homework_navigation/iOS_homework_navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_homework_navigation
+//
+//  Created by 김성은 on 18/04/2020.
+//  Copyright © 2020 김성은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
